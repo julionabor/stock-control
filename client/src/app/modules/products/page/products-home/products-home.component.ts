e7f56a1d67b8e494ae8086d20f83049c632ce945
Julio Nabor <julionabor@gmail.com>
2024-03-18 21:24:44 +0000

    Listando os produtos

---
+++ client/src/app/modules/products/page/products-home/products-home.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { ProductsDataTransferService } from 'src/app/services/products/products-data-transfer.service';
+import { ProductsService } from 'src/app/services/products/products.service';
+
+@Component({
+  selector: 'app-products-home',
+  templateUrl: './products-home.component.html',
+})
+export class ProductsHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public productsDatas: Array<GetAllProductsResponse> = [];
+
+  constructor(
+    private productsService: ProductsService,
+    private productsDtService: ProductsDataTransferService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getServiceProductsDatas();
+  }
+  getServiceProductsDatas() {
+    const productsLoaded = this.productsDtService.getProductsDatas();
+
+    if (productsLoaded.length > 0) {
+      this.productsDatas = productsLoaded;
+    } else {
+      this.getAPIProductsDatas();
+    }
+    console.log("dados:", this.productsDatas)
+  }
+  getAPIProductsDatas() {
+    this.productsService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsDatas = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.router.navigate(['/dashboard']);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos',
+            life: 2500,
+          });
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
