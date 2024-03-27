2cf9dcb43ba373e7941e55bf4f3c70ae54b878e9
Julio Nabor <julionabor@gmail.com>
2024-03-27 21:33:19 +0000

    Feat: Conclusao da implementação da Edição na UI

--- client/src/app/modules/products/components/products-form/products-form.component.ts
+++ client/src/app/modules/products/components/products-form/products-form.component.ts
@@ -7,7 +7,9 @@ import { Subject, takeUntil } from 'rxjs';
 import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/GetCategoriesResponse';
 import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { CreateProductRequest } from 'src/app/models/interfaces/products/request/CreateProductRequest';
+import { EditProductRequest } from 'src/app/models/interfaces/products/request/EditProductRequest';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { ProductEvent } from 'src/app/modules/enums/products/ProductEvent';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
 import { ProductsDataTransferService } from 'src/app/services/products/products-data-transfer.service';
 import { ProductsService } from 'src/app/services/products/products.service';
@@ -42,6 +44,10 @@ export class ProductsFormComponent implements OnInit, OnDestroy {
     amount: [0, Validators.required],
   });
 
+  public addProductAction = ProductEvent.ADD_PRODUCT_EVENT;
+  public editProductAction = ProductEvent.EDIT_PRODUCT_EVENT;
+  public saleProductAction = ProductEvent.SALE_PRODUCT_EVENT;
+
   constructor(
     private categoriesService: CategoriesService,
     private formBuilder: FormBuilder,
@@ -54,6 +60,16 @@ export class ProductsFormComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.productAction = this.ref.data;
+
+    if (
+      this.productAction?.event.action === this.editProductAction &&
+      this.productAction?.productDatas
+    ) {
+      this.getProductSelectedDatas(this.productAction?.event.id as string); // fazer um casting para garantir que o retorno é string
+    }
+    this.productAction?.event?.action === this.saleProductAction &&
+      this.getProductDatas();
+
     this.getAllCategories();
   }
   getAllCategories() {
@@ -109,7 +125,42 @@ export class ProductsFormComponent implements OnInit, OnDestroy {
     this.addProductsForm.reset();
   }
   handleSubmitEditProduct(): void {
-    if (this.editProductForm.value && this.editProductForm.valid) {
+    if (
+      this.editProductForm.value &&
+      this.editProductForm.valid &&
+      this.productAction.event.id
+    ) {
+      const requestEditProduct: EditProductRequest = {
+        name: this.editProductForm.value?.name as string,
+        price: this.editProductForm.value?.price as string,
+        product_id: this.productAction?.event?.id,
+        description: this.editProductForm.value?.description as string,
+        amount: Number(this.editProductForm.value?.amount),
+      };
+      this.productsService
+        .editProduct(requestEditProduct)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: () => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: 'Produto editado com sucesso',
+              life: 2500,
+            });
+            this.editProductForm.reset();
+          },
+          error: (err) => {
+            console.error(err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Ocorreu um erro ao editar produto!',
+              life: 2500,
+            });
+            this.editProductForm.reset();
+          },
+        });
     }
   }
   getProductSelectedDatas(productId: string): void {
